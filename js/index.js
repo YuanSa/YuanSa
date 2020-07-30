0b3c1cf4919c9ce443fb91faf8f3265ea9275217
Yang Zihan <yang.zihan@columabi.edu>
2020-07-30 00:25:28 -0400

    reframe

--- js/index.js
+++ js/index.js
@@ -1,22 +1,45 @@
-const app = new Vue({
-    el: "#page",
-    data: {
-        // personal: {
-        //     name,
-        //     email,
-        //     phone,
-        //     address,
-        // },
-        // links,
-        // experience,
+const resumeApp = {
+    data() {
+        return {
+            personal: {
+                // name: ``,
+                // email: ``,
+                // phone: ``,
+                // address: ``,
+            },
+            // links,
+            // experience,
+        };
     },
-    beforeCreate: async function () {
+    async beforeCreate() {
         await fetch("data/0.json")
-            .then(function (response) {
+            .then((response) => {
                 return response.json();
             })
-            .then(function (myJson) {
-                console.log(myJson);
+            .then((userData) => {
+                for (k in userData) {
+                    this[k] = userData[k];
+                }
+                // for (k in userData.personal) {
+                //     this.personal[k] = userData.personal[k];
+                // }
+
+                // this.personal.name = userData.personal.name;
+                // this.personal.email = userData.personal.email;
             });
     },
-});
+    computed: {},
+    methods: {},
+};
+
+function itemClass(item) {
+    if (item.subtitle) {
+        return "item_rich";
+    }
+    if (item.name) {
+        return "item_normal";
+    }
+    return "item_simple";
+}
+
+Vue.createApp(resumeApp).mount(`#page`);
